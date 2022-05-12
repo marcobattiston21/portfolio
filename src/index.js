399c6f0a07bf56d393547c290f59136383ddd7c5
Marco Battiston <76410842+marcobattiston21@users.noreply.github.com>
2022-05-12 20:29:59 +0200

    Creating function to link presentation to button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initTilt from "./js/tilt";
 import initSr from "./js/sr";
 import resume from "./assets/resume.pdf";
+import presentation_marketing_research from "./assets/presentation_marketing_research.pdf";
 
 import "./style/main.scss";
 
@@ -24,8 +25,16 @@ function addResume(pdf) {
   resumeButton.setAttribute("href", pdf);
 }
 
+function addPresentation(pdf) {
+  if (!pdf) return;
+
+  const presentationButton = document.querySelector(".cta-btn--presentation");
+  presentationButton.setAttribute("href", pdf);
+}
+
 initSr();
 initTilt();
 
 // uncomment this if you want to attach your resume
 addResume(resume);
+addPresentation(presentation_marketing_research);
